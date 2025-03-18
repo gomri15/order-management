b2bd41ab6ddb1f50d241f1d4a483559c859695ea
Omri Golan <gomri@github.com>
2025-03-18 08:12:24 +0200

    style: clean up main.py to only init fastapi

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import jwt
-from passlib.context import CryptContext
 from pydantic import BaseModel
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from app.api import users
+
+
+app = FastAPI(title="Order Management System", version="1.0")
+
 
 fake_users_db = {
     "johndoe": {
@@ -47,37 +48,15 @@ class UserInDB(User):
     hashed_password: str
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-app = FastAPI()
-
 db = {}
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
 
 
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    print(password, user.hashed_password)
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -119,31 +98,4 @@ def get_current_active_user(
     return current_user
 
 
-@app.post("/users/register")
-def register(user: User):
-    db[user.id] = user
-    return {"message": "User registered successfully"}
-
-
-@app.post("/token")
-def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user = authenticate_user(fake_db=fake_users_db,
-                             username=form_data.username,
-                             password=form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.username},
-                                       expires_delta=access_token_expires)
-    return Token(access_token=access_token, token_type="bearer")
-
-
-@app.get("/user", response_model=User)
-def get_user_details(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    return current_user
+app.include_router(users.router, prefix="/users", tags=["Users"])
