821b3ebdd4cf4dfa557fada43642f5d963cdad4c
Omri Golan <omri.golan@audiocodes.com>
2025-04-27 08:34:06 +0300

    feat(current user api): add current api endpoint for getting user profile details

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -1,9 +1,12 @@
 from typing import List
 import uuid
+
+from app.auth.dependencies import get_current_user
 from app.core.errors import NoChangeError
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.db.database import get_db
+from app.db.models import User
 from app.schemas.users import UserCreate, UserResponse, UserLogin, UserUpdate
 from app.services.users import UserService
 from app.core.security import get_security_service, SecurityService
@@ -17,9 +20,9 @@ class UserAPI:
                  response_model=UserResponse,
                  status_code=status.HTTP_201_CREATED)
     def register_user(
-        user: UserCreate,
-        db: Session = Depends(get_db),
-        security_service: SecurityService = Depends(get_security_service)
+            user: UserCreate,
+            db: Session = Depends(get_db),
+            security_service: SecurityService = Depends(get_security_service)
     ):
         user_service = UserService(db, security_service)
         existing_user = user_service.is_user_exists(user.email)
@@ -30,9 +33,9 @@ class UserAPI:
     @staticmethod
     @router.post("/login", response_model=dict)
     def login_user(
-        user: UserLogin,
-        db: Session = Depends(get_db),
-        security_service: SecurityService = Depends(get_security_service)
+            user: UserLogin,
+            db: Session = Depends(get_db),
+            security_service: SecurityService = Depends(get_security_service)
     ):
         user_service = UserService(db, security_service)
         token = user_service.authenticate_user(user)
@@ -43,10 +46,10 @@ class UserAPI:
     @staticmethod
     @router.put("/{user_id}")
     def update_user(
-        user_id: str,
-        update_data: UserUpdate,
-        db: Session = Depends(get_db),
-        security_service: SecurityService = Depends(get_security_service)
+            user_id: str,
+            update_data: UserUpdate,
+            db: Session = Depends(get_db),
+            security_service: SecurityService = Depends(get_security_service)
     ):
         user_service = UserService(db, security_service)
         try:
@@ -56,21 +59,18 @@ class UserAPI:
             raise HTTPException(status_code=200, detail="No changes to update")
 
     @staticmethod
-    @router.get("/{user_id}", response_model=UserResponse)
-    def get_user(
-        user_id: uuid.UUID,
-        db: Session = Depends(get_db),
-        security_service: SecurityService = Depends(get_security_service)
-    ):
-        user_service = UserService(db, security_service)
-        return user_service.get_user(user_id)
-
-    @staticmethod
     @router.get("/", response_model=List[UserResponse])
     # TODO: add filtering
     def get_users(
-        db: Session = Depends(get_db),
-        security_service: SecurityService = Depends(get_security_service)
+            db: Session = Depends(get_db),
+            security_service: SecurityService = Depends(get_security_service)
     ):
         user_service = UserService(db, security_service)
         return user_service.get_users()
+
+    @staticmethod
+    @router.get("/current", response_model=UserResponse)
+    def fetch_current_user(
+            user: User = Depends(get_current_user)  # Assuming get_current_user is defined elsewhere
+    ):
+        return user
